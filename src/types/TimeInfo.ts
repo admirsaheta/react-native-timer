6c12cbc34fe7bbfbea3503f9894122e7546180cc
Admir Šaheta <admirxsaheta@gmail.com>
2023-12-19 12:56:50 +0100

    feat/add-utilities

---
+++ src/types/TimeInfo.ts
@@ -0,0 +1,14 @@
+export interface TimeInfo {
+    totalSeconds: number;
+    seconds: number;
+    minutes: number;
+    hours: number;
+    days: number;
+};
+
+export interface FormattedTimeInfo {
+    seconds: number;
+    minutes: number;
+    hours: number;
+    ampm: string;
+};
---
+++ src/utils/MValidator.ts
@@ -0,0 +1,25 @@
+interface ExpiryTimestampValidation {
+    expiryTimestamp(expiryTimestamp: any): boolean;
+}
+
+interface OnExpireValidation {
+    onExpire(onExpire: any): boolean;
+}
+
+export default class Validate implements ExpiryTimestampValidation, OnExpireValidation {
+    expiryTimestamp(expiryTimestamp: any): boolean {
+        const isValid = (new Date(expiryTimestamp)).getTime() > 0;
+        if (!isValid) {
+            console.warn('react-native-timer: { useTimer } Invalid expiryTimestamp settings', expiryTimestamp);
+        }
+        return isValid;
+    }
+
+    onExpire(onExpire: any): boolean {
+        const isValid = onExpire && typeof onExpire === 'function';
+        if (onExpire && !isValid) {
+            console.warn('react-native-timer: { useTimer } Invalid onExpire settings function', onExpire);
+        }
+        return isValid;
+    }
+}
